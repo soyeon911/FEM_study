961cbffc1f19315b636e6c15097997a8a5cdd5ff
soyeon911 <sypark911@yonsei.ac.kr>
2025-01-12 03:18:28 +0900

    Load vector 생성 함수

--- MATLAB/FiniteElement1D/SourceAssembler1D.m
+++ MATLAB/FiniteElement1D/SourceAssembler1D.m
@@ -1,5 +1,19 @@
 function b = SourceAssembler1D(x, f, kappa, g)
-b = LoadAssembler1D(x, f); % 기본 로드 벡터
-b(1) = b(1) + kappa(1) * g(1); % 왼쪽 경계 데이터 추가
-b(end) = b(end) + kappa(2) * g(2); % 오른쪽 경계 데이터 추가
+    n = length(x) - 1; % 구간 개수
+    b = zeros(n + 1, 1); % 로드 벡터 초기화
+
+    % 내부 열원 계산
+    for i = 1:n
+        h = x(i + 1) - x(i); % 현재 구간 길이
+        xmid = (x(i + 1) + x(i)) / 2; % 구간 중간점
+        fmid = f(xmid); % 열원 값
+
+        % 중간점 구적법을 사용하여 로드 벡터 계산
+        b(i) = b(i) + fmid * h / 2;
+        b(i + 1) = b(i + 1) + fmid * h / 2;
+    end
+
+    % 경계 데이터 추가
+    b(1) = b(1) + kappa(1) * g(1); % 왼쪽 경계
+    b(end) = b(end) + kappa(2) * g(2); % 오른쪽 경계
 end
